4db1e07c0c082f27ad27b32e6af6fafcd7fd0645
Jamma003 <jamma003@gmail.com>
2024-12-20 15:02:38 +0000

    Initial commit

---
+++ src/main/java/net/jamma003/skeletonarrownerf/SkeletonArrowNerfClient.java
@@ -0,0 +1,10 @@
+package net.jamma003.skeletonarrownerf;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkeletonArrowNerfClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
